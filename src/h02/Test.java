acc321fe1339ee84d773d833500b6c68f6e0d4d7
Douwe <2090727@talnet.com>
2020-09-09 16:48:38 +0200

    Change text from "Hallo klas 8a" to "Hallo Cohort 0!"

--- src/h02/Test.java
+++ src/h02/Test.java
@@ -4,11 +4,13 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class Test extends Applet {
-
+    //Hier kan je commentaar/uitleg schrijven zonder dat de code dat leest.
     public void init() {
+        setBackground(Color.cyan);
     }
 
     public void paint(Graphics g) {
+        g.setColor(Color.black);
         g.drawString("Hallo daapi's",50,50);
     }
 }
