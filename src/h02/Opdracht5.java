3b0cdc4534d73e804dfd2d22a85cbfbcdc1cfd5b
Douwe <2090727@talnet.com>
2020-09-11 10:35:42 +0200

    Huis, deur & één raam opdracht uitgevoerd en toegevoegd.

--- src/h02/Opdracht5.java
+++ src/h02/Opdracht5.java
@@ -6,15 +6,15 @@ import java.awt.*;
 public class Opdracht5 extends Applet {
 
     public void init() {
-        setBackground(Color.WHITE);
-        resize(250,250);
+        setBackground(Color.BLACK);
+        resize(500,500);
     }
 
-    public void paint(Graphics G) {
-
-
-
-
+    public void paint(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRoundRect(25,50,50,50,0,0);
+        g.drawLine(25,50,50,25);
+        g.drawLine(75,50,50,25);
     }
 
 }
--- src/h02/Test.java
+++ src/h02/Test.java
@@ -12,16 +12,26 @@ public class Test extends Applet {
 
     public void paint(Graphics g) {
         g.setColor(Color.black);
-        g.drawString("Hallo daapi's",50,68);
-        g.drawLine(50,70,270,70);
+        g.drawString("Hallo daapi's",100,68);
+        g.drawLine(100,70,270,70);
+        g.setColor(Color.WHITE);
         g.fillRoundRect(10,100,110,110,50,50);
+        g.setColor(Color.BLACK);
         g.drawOval(125,100,50,100);
         g.fillArc(175,150,100,50,90,90);
+       //Driehoek
         g.drawLine(225,250,275,250);
-        g.drawLine(225,250,250,225); //Code's voor driehoek.
+        g.drawLine(225,250,250,225);
         g.drawLine(275,250,250,225);
-
         g.drawLine(225,150,225,150);
+        //Huis
+        g.setColor(Color.BLACK);
+        g.fillRoundRect(25,50,50,50,0,0);
+        g.drawLine(25,50,50,25);
+        g.drawLine(75,50,50,25);
+        g.setColor(Color.WHITE);
+        g.fillRoundRect(30,85,7,20,0,0);
+        g.fillRoundRect(47,85,23,7,0,0);
 
     }
 
