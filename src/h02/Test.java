e5c788880e6ffce8fc9711409724dc3c73105b7f
Douwe <2090727@talnet.com>
2020-09-11 12:02:43 +0200

    Grafiek met gewichten van Valerie, Jeroen & Hans toegevoegd & uitgevoerd.

--- src/h02/Test.java
+++ src/h02/Test.java
@@ -7,7 +7,7 @@ public class Test extends Applet {
     //Hier kan je commentaar/uitleg schrijven zonder dat de code dat leest.
     public void init() {
         setBackground(Color.cyan);
-        resize(500,500);
+        resize(750,750);
     }
 
     public void paint(Graphics g) {
@@ -43,9 +43,34 @@ public class Test extends Applet {
         g.fillRoundRect(226,35,25,5,0,0);
         //Staaf diagram
         g.setColor(Color.BLACK);
-        g.fillRoundRect(50,300,200,200,0,0);
-
-
+        g.fillRoundRect(50,275,200,200,0,0);
+        g.setColor(Color.WHITE);
+        g.drawLine(70,450,225,450);
+        g.drawLine(75,450,75,275);
+        g.drawLine(125,450,125,275);
+        g.drawLine(175,450,175,275);
+        g.drawLine(225,450,225,275);
+        g.drawLine(70,430,75,430);
+        g.drawLine(70,410,75,410);
+        g.drawLine(70,390,75,390);
+        g.drawLine(70,370,75,370);
+        g.drawLine(70,350,75,350);
+        g.setColor(Color.white);
+        g.drawString("0",63,450);
+        g.drawString("20",57,430);
+        g.drawString("40",57,410);
+        g.drawString("60",57,390);
+        g.drawString("80",57,370);
+        g.drawString("100",50,350);
+        g.drawString("Valerie",80,470);
+        g.drawString("Jeroen",130,470);
+        g.drawString("Hans",185,470);
+        g.setColor(Color.pink);
+        g.fillRoundRect(76,410,49,40,0,0);
+        g.setColor(Color.GREEN);
+        g.fillRoundRect(126,350,49,100,0,0);
+        g.setColor(Color.blue);
+        g.fillRoundRect(176,370,49,80,0,0);
 
 
     }
