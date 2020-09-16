5467e56943b2a47ae84602d1f5dbd9c6179cd5d2
Douwe <2090727@talnet.com>
2020-09-16 13:19:22 +0200

    Opdracht 6.3 om even opteslaan, 6.3 niet compleet afgerond.

---
+++ src/h06/Opdracht6_3.java
@@ -0,0 +1,23 @@
+package h06;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht6_3 extends Applet {
+
+    int positief1;
+    int positief2;
+    int antwoord;
+    public void init(){
+        setBackground(Color.CYAN);
+        positief1 = 343;
+        positief2 = 432;
+        antwoord = positief1 + positief2;
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.BLACK);
+        String antwoordString = positief1 + "+" + positief2 + "=" + antwoord;
+        g.drawString(antwoordString,50,50);
+    }
+}
