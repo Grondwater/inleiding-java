b9c24a362df36fcd5fc5109894e898535db1961e
Douwe <2090727@talnet.com>
2020-09-17 09:55:05 +0200

    Opdracht/Hoofdstuk 7 toegevoegd & uitgevoerd.

--- src/h07/Opdracht7.java
+++ src/h07/Opdracht7.java
@@ -14,5 +14,6 @@ public class Opdracht7 extends Applet {
     public void paint(Graphics g){
     g.setColor(Color.BLACK);
     g.drawString("Mogguh maat",50,50);
+    g.drawString("De klasse die word gebruikt is volgens mij Show.",50,70);
 }
 }
