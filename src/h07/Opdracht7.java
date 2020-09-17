b0b4999b591cb02f9bccfefde4fac864ca4af4ac
Douwe <2090727@talnet.com>
2020-09-17 09:47:47 +0200

    Opdracht/Hoofdstuk 7 toegevoegd. (map niet de opdracht)

---
+++ src/h07/Opdracht7.java
@@ -0,0 +1,18 @@
+package h07;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht7 extends Applet {
+
+
+
+    public void init(){
+    setBackground(Color.CYAN);
+    setSize(500,500);
+}
+    public void paint(Graphics g){
+    g.setColor(Color.BLACK);
+    g.drawString("Mogguh maat",50,50);
+}
+}
