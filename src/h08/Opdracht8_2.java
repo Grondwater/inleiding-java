9323a2a03aa9d68165dfccc2b33dbe718324b3c3
Douwe <2090727@talnet.com>
2020-10-01 11:47:04 +0200

    Hoofdstuk 8; Applet werkt.

--- src/h08/Opdracht8_2.java
+++ src/h08/Opdracht8_2.java
@@ -23,7 +23,7 @@ public class Opdracht8_2 extends Applet {
     public void init() {
         setBackground(Color.CYAN);
 
-        counter = 0 ;
+        counter = 1 ;
         counter1 = 1;
 
         man = new Button("Man");
@@ -44,19 +44,40 @@ public class Opdracht8_2 extends Applet {
 
         manCounter = new Label("Mannen " + counter);
                 add(manCounter);
+
+        vrouwCounter = new Label("Vrouwen " + counter);
+                add(vrouwCounter);
+
+        jongenCounter = new Label("Jongens " + counter);
+                add(jongenCounter);
+
+        meisjeCounter = new Label("Meisjes " + counter);
+                add(meisjeCounter);
     }
     
     public void paint(Graphics g){
 
-        manCounter.setLocation(50,100);
+        manCounter.setLocation(50,75);
         manCounter.setName("Deelnames " + counter);
-        manCounter.setSize(150,50);
+        manCounter.setSize(150,30);
+
+        vrouwCounter.setLocation(50,105);
+        vrouwCounter.setName("Deelnames " + counter);
+        vrouwCounter.setSize(150,30);
+
+        jongenCounter.setLocation(50,135);
+        jongenCounter.setName("Deelnames " + counter);
+        jongenCounter.setSize(150,30);
+
+        meisjeCounter.setLocation(50,165);
+        meisjeCounter.setName("Deelnames " + counter);
+        meisjeCounter.setSize(150,30);
     }
 
     class ManListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
-            manCounter.setText("Mannen " + counter1);
+            manCounter.setText("Mannen "  + counter);
             repaint();
             String message = manCounter.getText();
             counter++;
@@ -67,6 +88,10 @@ public class Opdracht8_2 extends Applet {
     class VrouwListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
+            vrouwCounter.setText("Vrouwen "  + counter);
+            repaint();
+            String message = vrouwCounter.getText();
+            counter++;
 
 
         }
@@ -76,6 +101,10 @@ public class Opdracht8_2 extends Applet {
     class JongenListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
+            jongenCounter.setText("Jongens "  + counter);
+            repaint();
+            String message = jongenCounter.getText();
+            counter++;
 
 
         }
@@ -85,6 +114,10 @@ public class Opdracht8_2 extends Applet {
     class MeisjeListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
+            meisjeCounter.setText("Meisjes "  + counter);
+            repaint();
+            String message = meisjeCounter.getText();
+            counter++;
 
 
         }
