b3c2a84e09324e2a6f292c4211aa9acb726e4e70
Douwe <2090727@talnet.com>
2020-11-04 09:35:15 +0100

    Hoofdstuk 12; Opdracht12 5 & 6 toegevoegd en afgerond;

--- src/h12/Opdracht12_3.java
+++ src/h12/Opdracht12_3.java
@@ -1,9 +1,28 @@
 package h12;
+import java.awt.*;
+import java.applet.*;
 
-import java.applet.Applet;
+public class Opdracht12_3 extends Applet  {
+    double[] salary;
+    double[] copy;
 
-public class Opdracht12_3 extends Applet {
+    public void init() {
+        salary = new double[5];
+        copy = new double[5];
 
+        for (int counter = 0; counter < salary.length; counter ++) {
+            salary[counter] = 100 * counter + 100;
+        }
+        for (int counter = 0; counter < salary.length; counter ++) {
+            copy[counter] = salary[counter];
+        }
+        copy[3] = 1000;
+    }
 
-
+    public void paint(Graphics g) {
+        for (int counter = 0; counter < salary.length; counter ++) {
+            g.drawString("" + copy[counter], 50, 20 * counter + 20);
+            g.drawString("" + salary[counter], 100, 20 * counter + 20);
+        }
+    }
 }
